2061b4508c5a0ba38a4cfee28ab5c0059b93c5bf
apollobing <taras.career@gmail.com>
2023-06-29 11:56:38 +0200

    5.2. Создать программу, вычисляющую факториал. [#193] - Fix: removed unnecessary if else statement, start for loop from 2

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -4,12 +4,8 @@ public class Factorial {
     public static int calc(int n) {
         int result = 1;
 
-        if (n == 0) {
-            return result;
-        } else {
-            for (int i = 1; i <= n; i++) {
-                result *= i;
-            }
+        for (int i = 2; i <= n; i++) {
+            result *= i;
         }
 
         return result;
