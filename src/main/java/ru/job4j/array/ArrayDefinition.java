29cbfc065d0f6adf7de10442c8c96f7c9d5635f7
apollobing <taras.career@gmail.com>
2023-06-30 13:23:47 +0200

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,7 +4,18 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
+        String[] names = new String[4];
         float[] prices = new float[40];
+
+        names[0] = "Petr Arsentev";
+        names[1] = "Korobeinikov Stas";
+        names[2] = "Esipov Alexey";
+        names[3] = "Andrei Hincu";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
 
         System.out.println(ages.length);
         System.out.println(surnames.length);
