68550aee8d8ed985553b39764ddeee7a9ec8abca
apollobing <taras.career@gmail.com>
2023-06-30 11:46:01 +0200

    5.7. Ипотека [#156315] - Fix: debt recalculation placed inside while loop, removed unnecessary debt variable

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double debt = amount + percent / 100 * amount;
-        while (debt > 0) {
-            debt -= salary;
+        while (amount > 0) {
+            amount = amount + percent / 100 * amount;
+            amount -= salary;
             year++;
         }
         return year;
