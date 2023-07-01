f3f2c0d32d3ab330a1bfee9a3331a19cf4cb1ff5
apollobing <taras.career@gmail.com>
2023-07-01 12:07:24 +0200

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,13 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
