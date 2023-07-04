38dc76db44bbc8fe009e5a534e1f5f210bc79e52
apollobing <taras.career@gmail.com>
2023-07-04 12:21:43 +0200

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741] - Fix: removed unnecessary variable, changed name of variable in for loop, change body of while loop

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -7,11 +7,10 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
-        int change = money - price;
-        for (int c : coins) {
-            while (change >= c) {
-                change -= c;
-                rsl[size] = c;
+        for (int coin : coins) {
+            while (money - price >= coin) {
+                money -= coin;
+                rsl[size] = coin;
                 size++;
             }
         }
