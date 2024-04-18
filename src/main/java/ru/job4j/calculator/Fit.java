e369d35b6ea2b65e463964a9e1b577a1601bdcad
apollobing <taras.career@gmail.com>
2024-04-18 13:23:39 +0200

    Codium. Нейросети спешат на помощь. [#505152]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,14 +1,19 @@
 package ru.job4j.calculator;
 
 public class Fit {
+
+    private static final short BASE_HEIGHT_MAN = 100;
+    private static final short BASE_HEIGHT_WOMAN = 110;
+    private static final double WEIGHT_COEFFICIENT = 1.15;
+
     public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;
-        return rsl;
+        double result = (height - BASE_HEIGHT_MAN) * WEIGHT_COEFFICIENT;
+        return result;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
-        return rsl;
+        double result = (height - BASE_HEIGHT_WOMAN) * WEIGHT_COEFFICIENT;
+        return result;
     }
 
     public static void main(String[] args) {
